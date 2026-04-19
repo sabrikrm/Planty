bdd01e1cfff7371fc7c11c1d800cf90c2006f296
Sabri Karoumi <skaroumi71@gmail.com>
2026-04-19 18:08:29 +0200

    Ajout du theme enfant et nettoyage du projet

--- wp-content/themes/planty-enfant/header.php
+++ wp-content/themes/planty-enfant/header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;700;800&display=swap" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
